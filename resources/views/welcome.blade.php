c12a6f3001fa06666d56db026e479307e293a6b0
Mohammed <ghamma43@gmail.com>
2026-02-11 03:21:15 +0300

    First com: lab 02

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moe</title>
+</head>
+<body>
+    <h1>
+        Hello, me project, me good...
+    </h1>
+</body>
+</html>
